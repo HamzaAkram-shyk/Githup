fa2d3b0f0aefda0ec7bab9090710daf0c3191293
HamzaAkram <hamzaakram.shyk17@gmail.com>
2021-08-07 14:56:47 +0500

    New Feature

--- app/src/main/java/com/example/githup/MainActivity.kt
+++ app/src/main/java/com/example/githup/MainActivity.kt
@@ -3,15 +3,31 @@ package com.example.githup
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var value: Response<List<Boolean>>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         println("This is remote base")
         println("Helllo there")
-        Log.d("","")
+        Log.d("", "")
+        value = Response.Success<List<Boolean>>(listOf(true, false))
+        value = Response.Loading()
+        value = Response.Error<List<Boolean>>(null, "Error")
+        when (value) {
+            is Response.Success -> {
 
+            }
+            is Response.Loading -> {
+
+            }
+            is Response.Error -> {
+
+            }
+        }
         println("My New Commit")
         println("Add new Feature")
 
